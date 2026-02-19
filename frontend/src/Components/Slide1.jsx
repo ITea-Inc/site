8ef717d6e6270161b516a00cfcb331a8c05e47b8
mllwrl <kiemie71@yandex.ru>
2026-02-19 09:52:48 +0400

    Skide 1, TODO slide1(

--- frontend/src/Components/Slide1.jsx
+++ frontend/src/Components/Slide1.jsx
@@ -15,28 +15,33 @@ const Slide1 = ({ scrollProgress = 0, slideDirection = 'next', isTransitioning =
             Узнать больше
           </button>
 
-          <div className="slide1-video">
-            <video autoPlay muted loop={false} playsInline>
-              <source src="/videos/video.mp4" type="video/mp4" />
-            </video>
-          </div>
+         
         </div>
 
         <div className="slide1-right">
           <div className="slide1-tiles">
             <div className="tile slide1-tile-process">
-              <div className="tile-title">Повышение процессов</div>
+              <div className="tile-photo-Title">
+                <img className='settingsPhoto' src='images/settings-svgrepo-com.svg'></img>
+                <div className="tile-title">Повышение процессов</div>
+              </div>
               <div className="tile-description">Эффективное и безошибочное выполнение задач благодаря автоматизации</div>
             </div>
 
             <div className="tile slide1-tile-scalable">
-              <h4>Масштабируемые решения</h4>
-              <p>Гибкие IT-решения, которые растут вместе с вашим бизнесом.</p>
+              <div className="tile-photo-Title">
+                <img className='settingsPhoto' src='images/chart-growth-invest-svgrepo-com.svg'></img>
+                <div className="tile-title">Масштабируемые решения</div>
+              </div>
+              <div className="tile-description">Гибкие IT-решения, которые растут вместе с вашим бизнесом</div>
             </div>
 
             <div className="tile slide1-tile-custom">
-              <h4>Индивидуальный подход</h4>
-              <p>Адаптируемся под уникальные потребности вашего бизнеса.</p>
+              <div className="tile-photo-Title">
+                <img className='settingsPhoto' src='images/businessman-svgrepo-com.svg'></img>
+                <div className="tile-title">Индивидуальный подход</div>
+              </div>
+              <div className="tile-description">Адаптируемся под уникальные потребности вашего бизнеса</div>
             </div>
           </div>
         </div>
